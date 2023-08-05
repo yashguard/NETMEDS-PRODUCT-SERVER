8a626a9ea17c119b71435ab1dd1eef0e710dcfec
Yash Guard <yashguard>
2023-08-05 20:11:34 +0530

    Again

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const connect = require("./config/db");
-const { treatments, diabities, covid, surgicals } = require("./models/product.schema");
+const {
+  treatments,
+  diabities,
+  covid,
+  surgicals,
+} = require("./models/product.schema");
 const server = express();
 server.use(express.json());
 server.use(cors());
 require("dotenv").config();
 let port = process.env.port;
+
+server.get("/", async (req, res) => {
+  res.send("Home page");
+});
 
 server.get("/covid", async (req, res) => {
   const products = await covid.find();
@@ -27,7 +36,6 @@ server.get("/treatments", async (req, res) => {
   const products = await treatments.find();
   res.send(products);
 });
-
 
 server.listen(port, () => {
   console.log(`Server is running`);
